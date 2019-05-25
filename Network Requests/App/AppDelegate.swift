38ce85a43e5f487884c9199fbff96cbcb31e8f3e
Denis Bystruev <dbystruev@me.com>
2019-05-25 20:19:39 +0300

    Rearranged project structure

---
+++ Network Requests/App/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Network Requests
+//
+//  Created by Denis Bystruev on 20/05/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
